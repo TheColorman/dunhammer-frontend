bd9401c43086b8e87f2ca59397e84bd82d7cf17e
Alexander Thoren <thoren.alex@gmail.com>
2021-12-22 20:05:35 +0100

    👽 Added authentication buttons

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
-
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export default function Header() {
+    const { data: session, status } = useSession()
+    const loading = status === "loading"
+    
     // Sticky banner with Dunhammer logo
     return (
-        <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 dark:bg-gray-900/75">
+        <div className="fixed top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-gray-900/10 dark:border-gray-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 dark:bg-gray-900/75">
             <div className="max-w-8xl mx-auto">
                 <div className="py-4 border-b border-gray-900/10 lg:px-8 lg:border-0 dark:border-gray-300/10 mx-4 lg:mx-0">
                     <div className="relative flex items-center">
@@ -34,15 +37,38 @@ export default function Header() {
                         <div className="relative flex items-center ml-auto">
                             <div className="flex items-center border-l border-gray-200 ml-6 pl-6 dark:border-gray-800">
                                 <div className="ml-6 block text-gray-400 hover:text-gray-500 dark:hover:text-gray-300">
-
-                                    <span className="sr-only">Login</span>
-                                    <Link href="/login">
-                                        <a>
-                                            <button className="btn btn-primary">
-                                                Login
-                                            </button>
-                                        </a>
-                                    </Link>
+                                    {!session && (
+                                        <>
+                                            <span className="sr-only">Login</span>
+                                            <a
+                                                href="/api/auth/signin"
+                                                onClick={(e) => {
+                                                    e.preventDefault()
+                                                    signIn("discord")
+                                                }}
+                                            >
+                                                <button className="btn btn-primary">
+                                                    Login
+                                                </button>
+                                            </a>
+                                        </>
+                                    )}
+                                    {session && (
+                                        <>
+                                            <span className="sr-only">Logout</span>
+                                            <a
+                                                href="/api/auth/signout"
+                                                onClick={(e) => {
+                                                    e.preventDefault()
+                                                    signOut()
+                                                }}
+                                            >
+                                                <button className="btn btn-primary">
+                                                    Logout
+                                                </button>
+                                            </a>
+                                        </>
+                                    )}
                                 </div>
                             </div>
                         </div>
