c109ac86625d0c910c3496701ed0dad8d1f032a3
Alexander Thoren <thoren.alex@gmail.com>
2021-12-22 21:25:06 +0100

    🎨 Added header to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link"
+import Header from "../components/header"
 
 export default function Page() {
     return (
         <>
             <div className="bg-gradient-to-br from-emerald-400 to-blue-400 dark:from-emerald-700 dark:to-blue-700 h-screen">
+                <Header />
                 <div className="relative flex flex-col items-center justify-center h-full">
                     <h1 className="text-9xl" >404</h1>
                     <Link href="/">
