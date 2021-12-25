a3e09601415a334b0558e9f41a32f3e19e19ac12
Alexander Thoren <thoren.alex@gmail.com>
2021-12-25 23:37:20 +0100

    👽 Changed user name to include discriminator

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,37 @@ import NextAuth from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import { Session } from "next-auth"
 
 const prisma = new PrismaClient()
 
+interface ExtendedSession extends Session {
+    user: {
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
+        discordId: string | null,
+        discriminator: string | null,
+    }
+}
+
+type DiscordUser = {
+    id: string;
+    username: string;
+    discriminator: string;
+    avatar: string;
+    bot?: boolean;
+    system?: boolean;
+    mfa_enabled?: boolean;
+    banner?: string;
+    accent_color?: number;
+    locale?: string;
+    verified?: boolean;
+    email?: string;
+    flags?: number;
+    premium_type?: 0 | 1 | 2;
+    public_flags?: number;
+}
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
@@ -24,6 +52,9 @@ export default NextAuth({
     },
     callbacks: {
         session: async ({ session, user, token }) => {
+
+            if (!session.user) return session
+
             // Get Discord token from database
             const discordToken = (await prisma.user.findUnique({
                 where: {
@@ -39,7 +70,7 @@ export default NextAuth({
             }))?.accounts[0].access_token
 
             if (!discordToken) return session
-
+            
             // Get user ID
             const url = "https://discord.com/api/v9/users/@me?"
             const response = await fetch(url, {
@@ -48,8 +79,11 @@ export default NextAuth({
                     "Content-Type": "application/json"
                 }
             })
-            const data = await response.json()
-            session.discordId = data.id
+            const data = (await response.json()) as DiscordUser
+            
+            const extSession = session as ExtendedSession
+            extSession.user.discordId = data.id
+            extSession.user.discriminator = data.discriminator
 
             return session
         }
