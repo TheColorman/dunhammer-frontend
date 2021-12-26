cdd32f767714cdbfea5924f65ac2bfb55864a11d
Alexander Thoren <thoren.alex@gmail.com>
2021-12-26 01:42:02 +0100

    🚧 Updated username display and added XP bar

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,10 +4,13 @@ import { signIn } from "next-auth/react";
 import Header from "../components/header";
 import Center from "../components/center";
 import Footer from "../components/footer";
+import { ExtendedSession } from "../lib/types";
+import { GetServerSideProps } from "next";
 
 export default function Dashboard() {
-    const { data: session, status } = useSession()
+    const { data, status } = useSession()
     const loading = status === 'loading'
+    const session = data as ExtendedSession
 
     if (loading) return (
         <>
@@ -45,17 +48,29 @@ export default function Dashboard() {
     return (
         <>
             <Header />
-            <div className="w-screen flex flex-col items-center pt-36 max-h-full">
-                <div>
-                <Image
-                    src={session.user?.image ?? "https://cdn.discordapp.com/embed/avatars/0.png"}
-                    alt="User Avatar"
-                    width={128}
-                    height={128}
-                    className="mt-8"
-                />
+            <div className="pt-36 flex justify-center">
+                <div className="card">
+                    <div>
+                    <Image
+                        src={session.user?.image ?? "https://cdn.discordapp.com/embed/avatars/0.png"}
+                        alt="User Avatar"
+                        width={128}
+                        height={128}
+                        className="mt-8"
+                        />
+                    </div>
+                    {session.user && (
+                        <h1 className="text-2xl">{session.user.name}<span className="text-xl text-gray-300">#{session.user.discriminator}</span></h1>
+                    )}
+                    <div className="flex flex-row items-end">
+                        <div className="mt-4 w-80 bg-black bg-opacity-20 h-5 rounded-full shadow-lg">
+                            <div className="bg-lime-600 h-5 w-[25%] rounded-full"></div>
+                        </div>
+                        <div>
+                            <p className="ml-2 text-white">+100 coins</p>
+                        </div>
+                    </div>
                 </div>
-                <h1 className="text-2xl">{session.user?.name ?? session.user?.email ?? "Hello!"}</h1>
             </div>
             <Footer />
         </>
