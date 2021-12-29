d27b3f721aa02bef71ebdccf35780ebcc91204d0
Alexander Thoren <thoren.alex@gmail.com>
2021-12-29 23:26:37 +0100

    🔒 Limited accessible data by always returning current user

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import query from '../../lib/db'
+import { ExtendedSession } from '../../lib/types'
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse  
 ) {
-    const session = await getSession({ req })
+    const session = (await getSession({ req })) as ExtendedSession
 
     if (!session) {
         res.status(401).json({ error: 'Not authenticated' })
@@ -16,7 +17,7 @@ export default async function handler(
     try {
         const result = await query({
             query: "SELECT * FROM `users` WHERE `id` = ?",
-            values: [req.body.id]
+            values: [session.user.discordId]
         })
         res.status(200).json(result)
     } catch (error) {
