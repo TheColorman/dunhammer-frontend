cb1ce24fff5235559e88afb8849559bdffde5da3
Alexander Thoren <thoren.alex@gmail.com>
2021-12-30 15:20:49 +0100

    🚧 Added component for background shop

---
+++ components/backgroundSale.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image"
+import useSWR from "swr"
+import { shopBackgrounds } from "../lib/types"
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+export default function BackgroundSale() {
+    const { data: apiData, error: apiError } = useSWR('/api/shop/background', fetcher)
+    const error = apiError || apiData?.error
+
+    if (error) return <h1 className="text-lg">Failed to load shop information</h1>
+    if (!apiData) return (
+        <div className="flex flex-row items-baseline">
+            <div className="spinner" role="status">
+                <span className="hidden">Loading...</span>
+            </div>
+        </div>
+    )
+    
+    const data = apiData as shopBackgrounds
+
+    return (
+        <>
+            {data.collections.map((collection) => (
+                <div key={collection.id}>
+                    <h1 className="text-lg">{collection.name}</h1>
+                    <h2 className="text-base">{collection.description}</h2>
+                    <div className="grid grid-cols-3 auto-rows-max gap-8">
+                        {collection.backgrounds.map((background) => (
+                            <div key={background.id} className="card" style={{alignItems: 'start'}}>
+                                <Image
+                                    src={background.image}
+                                    alt="background"
+                                    width={300}
+                                    height={300}
+                                />
+                                <p className="text-base">{background.name}</p>
+                                <p className="text-sm">{background.description}</p>
+                                <div className="bg-black/50 rounded-b-3xl flex w-[calc(100%+4rem)] h-12 -ml-8 -mb-8 mt-4 items-center justify-between">
+                                    <p className="text-yellow-500 ml-8 inline-flex items-baseline">{background.price}
+                                    <div className="self-center w-5 h-5 ml-1">
+                                        <Image
+                                            src="/images/DunhammerCoin.png"
+                                            alt="Dunhammer Coin"
+                                            width={16}
+                                            height={16}
+                                        />
+                                    </div>
+                                    </p>
+                                    <button className="bg-lime-600 text-white px-4 py-2 h-full rounded-br-3xl -mb-[2px] -mr-[2px] right-0">Buy</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </>
+    )
+}
