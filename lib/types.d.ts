0c730643011cd7679f23627103c95b8335f589b6
Alexander Thoren <thoren.alex@gmail.com>
2022-01-22 22:29:21 +0100

    🗃 Added AccessTokenResponse type

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -71,3 +71,11 @@ export type shopBackgrounds = {
 export type apiError = {
     error: string;
 }
+
+export type AccessTokenResponse = {
+    access_token: string;
+    token_type: "Bearer";
+    expires_in: number;
+    refresh_token: string;
+    scope: string;
+}
