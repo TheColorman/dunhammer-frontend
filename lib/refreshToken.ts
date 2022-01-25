7119e5d3f682c4410ee683d9aaf47d75af29e2c2
Alexander Thoren <thoren.alex@gmail.com>
2022-01-25 19:28:41 +0100

    ♻ Created deleteUser function

--- lib/refreshToken.ts
+++ lib/refreshToken.ts
@@ -3,6 +3,18 @@ import { PrismaClient } from "@prisma/client"
 import { AccessTokenResponse, APIError } from "./types"
 
 const prisma = new PrismaClient()
+
+async function deleteUser(userId: string) {
+    return await prisma.user.delete({
+        where: {
+            id: userId,
+        },
+        include: {
+            accounts: true,
+        }
+    })
+}
+
 export default async function refreshToken(user: User): Promise<{ token: string } | { error: string }> {
     // Get user account
     const userAccount = (await prisma.user.findUnique({
@@ -15,13 +27,19 @@ export default async function refreshToken(user: User): Promise<{ token: string 
     }))?.accounts[0]
 
     // Return if no account
-    if (!userAccount) return { error: "No account found" }
+    if (!userAccount) {
+        deleteUser(user.id)
+        return { error: "No account found" }
+    }
 
     // Return if token not expired
     const tokenExpiresAt = userAccount.expires_at
     if (tokenExpiresAt && tokenExpiresAt * 1000 > Date.now()) return userAccount.access_token ? { token: userAccount.access_token } : { error: "No token found" }
     // Return if no refresh token
-    if (!userAccount.refresh_token) return { error: "No refresh token found" }
+    if (!userAccount.refresh_token) {
+        deleteUser(user.id)
+        return { error: "No refresh token found" }
+    }
 
 
     const data = (await (await fetch(`https://discord.com/api/v8/oauth2/token`, {
@@ -39,15 +57,7 @@ export default async function refreshToken(user: User): Promise<{ token: string 
 
     // Delete member from database if API error
     if ("error" in data) {
-        await prisma.user.delete({
-            where: {
-                id: user.id,
-            },
-            include: {
-                accounts: true,
-                sessions: true,
-            }
-        })
+        deleteUser(user.id)
         return { error: data.error }
     }
 
@@ -65,7 +75,7 @@ export default async function refreshToken(user: User): Promise<{ token: string 
                     data: {
                         access_token: data.access_token,
                         token_type: data.token_type,
-                        expires_at: data.expires_in + Date.now() / 1000,
+                        expires_at: Math.floor((data.expires_in + Date.now() * 1000) / 1000),
                         refresh_token: data.refresh_token,
                         scope: data.scope,
                     }
