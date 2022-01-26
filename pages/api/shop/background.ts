e44283d4a2c72e3bedf4cc602e62500764b136c8
Alexander Thoren <thoren.alex@gmail.com>
2022-01-26 22:45:57 +0100

    :fire: Removed template response

--- pages/api/shop/background.ts
+++ pages/api/shop/background.ts
@@ -1,88 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-const response = {
-    collections: [
-        {
-            id: 1,
-            name: "Collection 1",
-            description: "This is a collection",
-            backgrounds: [
-                {
-                    id: 1,
-                    name: "Background 1",
-                    description: "This is a background in collection 1",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
 
-                }, {
-                    id: 2,
-                    name: "Background 2",
-                    description: "This is a background in collection 1",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
-                }, {
-                    id: 3,
-                    name: "Background 3",
-                    description: "This is a background in collection 1",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
-                }, {
-                    id: 4,
-                    name: "Background 4",
-                    description: "This is a background in collection 1",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
-                }, {
-                    id: 5,
-                    name: "Background 5",
-                    description: "This is a background in collection 1",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
-                }, {
-                    id: 6,
-                    name: "Background 6",
-                    description: "This is a background in collection 1",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
                 }
-            ]
-        }, {
-            id: 2,
-            name: "Collection 2",
-            description: "This is a collection",
-            backgrounds: [
-                {
-                    id: 1,
-                    name: "Background 1",
-                    description: "This is a background in collection 2",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
-                }, {
-                    id: 2,
-                    name: "Background 2",
-                    description: "This is a background in collection 2",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
-                }, {
-                    id: 3,
-                    name: "Background 3",
-                    description: "This is a background in collection 2",
-                    price: 1000,
-                    thumbnail: "https://i.imgur.com/wSTFkRM.png",
-                    image: "https://i.imgur.com/wSTFkRM.png",
                 }
-            ]
         }
-    ],
-}
 
 export default async function handler(
     req: NextApiRequest,
