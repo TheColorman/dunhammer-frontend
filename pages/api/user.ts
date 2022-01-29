e3b6ef41aaf64896eee2d74bce04ff24f159b04e
Alexander Thoren <thoren.alex@gmail.com>
2022-01-29 01:18:13 +0100

    🗃 Added user backgrounds as property on user object

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,7 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import query from '../../lib/db'
-import { DBUser, ExtendedSession } from '../../lib/types'
+import { DBUser, ExtendedSession, DBBackground } from '../../lib/types'
+
+interface M_DBUser extends DBUser {
+    user_id: string;
+}
+
+interface M_DBBackground extends DBBackground {
+    background_id: number;
+}
 
 export default async function handler(
     req: NextApiRequest,
@@ -21,11 +29,16 @@ export default async function handler(
 
     try {
         const result = ((await query({
-            query: "SELECT * FROM `users` WHERE `id` = ?",
+            query: "SELECT `usr`.`id` as `user_id`, `usr`.`username`, `usr`.`tag`, `usr`.`levelMentions`, `usr`.`levelDm`, `usr`.`disabled`, `usr`.`xp`, `usr`.`level`, `usr`.`coins`, `usr`.`spentMoney`, `usr`.`commandCount`, `usr`.`pingCount`, `usr`.`inviteCount`, `bg`.`id` as `background_id`, `bg`.`name`, `bg`.`description`, `price`, `thumbnail`, `image` FROM `users` as `usr` JOIN `user_background` as `usr_bg` ON `usr_bg`.`user_id` = `usr`.`id` JOIN `backgrounds` as `bg` ON `bg`.`id` = `usr_bg`.`background_id` WHERE `usr`.`id` = ?",
             values: [session.user.discordId]
-        })) as Array<DBUser>)[0]
-        
-        res.status(200).json(result)
+        })) as Array<M_DBUser & M_DBBackground>)
+
+        // Extract all users backgrounds
+        const backgrounds = result.map(({ background_id, name, description, price, thumbnail, image }) => ({ id: background_id, name, description, price, thumbnail, image }))
+        // Extract user and add backgrounds as property
+        const user = (({ user_id, username, tag, levelMentions, levelDm, disabled, xp, level, coins, spentMoney, commandCount, pingCount, inviteCount }, backgrounds) => ({ id: user_id, username, tag, levelMentions, levelDm, disabled, xp, level, coins, spentMoney, commandCount, pingCount, inviteCount, backgrounds }))(result[0], backgrounds)
+
+        res.status(200).json(user)
     } catch (error) {
         res.status(500).json({ error })
     }
