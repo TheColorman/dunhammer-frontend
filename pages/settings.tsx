a004252ead2130fb447485ff0dddb33463f7af64
Alexander Thoren <thoren.alex@gmail.com>
2022-02-14 22:56:05 +0100

    :construction: Set up settings page layout

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,17 +1,43 @@
 import Header from "../components/header"
-import Center from "../components/center"
 import Footer from "../components/footer"
+import Image from "next/image"
 
 export default function Settings() {
   return (
     <>
       <Header />
-      <Center className="text-center">
-        <h1 className="text-4xl">The settings page</h1>
-        <p className="text-gray-600 dark:text-gray-400">
-          You&apos;ll be able to change your settings soon enough.
-        </p>
-      </Center>
+      <section className="pt-20 w-full h-full text-white flex pr-[18px]">
+        <div className="w-max bg-black/50 p-2">
+          <ul className="p-2 flex flex-col items-center">
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => {
+              return i == 0 ? (
+                <li key={i} className="my-3 w-[72px] h-[72px] border-4 border-pink-500 rounded-full">
+                  <Image
+                    src="/images/icon.svg"
+                    alt="Dunhammer Logo"
+                    width={64}
+                    height={64}
+                  />
+                </li>
+              ) : (
+                <li key={i} className="my-3 w-16 h-16">
+                  <Image
+                    src="/images/icon.svg"
+                    alt="Dunhammer Logo"
+                    width={64}
+                    height={64}
+                  />
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className="m-8 rounded-2xl sticky w-full bg-black/50 flex justify-center items-center">
+            <div className="m-2 bg-black">
+              what
+            </div>
+        </div>
+      </section>
       <Footer />
     </>
   )
