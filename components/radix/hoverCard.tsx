2d296375a8852f54fde71edcc5bc006fd1c841a8
Alexander Thoren <thoren.alex@gmail.com>
2022-02-14 21:32:37 +0100

    :sparkles: Added custom hoverCard component

---
+++ components/radix/hoverCard.tsx
@@ -0,0 +1,62 @@
+import * as HoverCardPrimitive from '@radix-ui/react-hover-card'
+import { ReactNode } from 'react';
+
+function HoverCard({ defaultOpen, open, onOpenChange, openDelay, closeDelay, children }: { defaultOpen?: boolean, open?: boolean, onOpenChange?: (open: boolean) => void, openDelay?: number, closeDelay?: number, children?: ReactNode }) {
+    return (
+        <HoverCardPrimitive.Root
+            defaultOpen={defaultOpen}
+            open={open}
+            onOpenChange={onOpenChange}
+            openDelay={openDelay}
+            closeDelay={closeDelay}
+        >
+            {children}
+        </HoverCardPrimitive.Root>
+    )
+}
+
+function Trigger({ asChild, children, className }: { asChild?: boolean, children?: ReactNode, className?: string }) {
+    return (
+        <HoverCardPrimitive.Trigger
+            asChild={asChild}
+            className={className}
+        >
+            {children}
+        </HoverCardPrimitive.Trigger>
+    )
+}
+
+function Content({ asChild, forceMount, portalled, side, sideOffset, align, alignOffset, avoidCollisions, collisionTolerance, children, className }: { asChild?: boolean, forceMount?: true | undefined, portalled?: boolean, side?: "top" | "right" | "bottom" | "left", sideOffset?: number, align?: "start" | "center" | "end", alignOffset?: number, avoidCollisions?: boolean, collisionTolerance?: number, children?: ReactNode, className?: string }) {
+    return (
+        <HoverCardPrimitive.Content
+            asChild={asChild}
+            forceMount={forceMount}
+            portalled={portalled}
+            side={side}
+            sideOffset={sideOffset}
+            align={align}
+            alignOffset={alignOffset}
+            avoidCollisions={avoidCollisions}
+            collisionTolerance={collisionTolerance}
+            className={className}
+        >
+            {children}
+        </HoverCardPrimitive.Content>
+    )
+}
+
+function Arrow({ asChild, width, height, offset, children, className }: { asChild?: boolean, width?: number, height?: number, offset?: number, children?: ReactNode, className?: string }) {
+    return (
+        <HoverCardPrimitive.Arrow
+            asChild={asChild}
+            width={width}
+            height={height}
+            offset={offset}
+            className={className}
+        >
+            {children}
+        </HoverCardPrimitive.Arrow>
+    )
+}
+
+export { HoverCard, Trigger, Content, Arrow }
