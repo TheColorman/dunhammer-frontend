96a5a01875327f0c8983fc1481699593255b226a
Alexander Thoren <thoren.alex@gmail.com>
2022-02-16 23:25:04 +0100

    🗃 Added user ID to session object

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -2,6 +2,7 @@ import { Session } from "next-auth"
 
 export interface ExtendedSession extends Session {
     user: {
+        id?: string;
         name?: string | null;
         email?: string | null;
         image?: string | null;
