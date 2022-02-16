60c5666183f50813a4573eeeb5a7daa7ecf95fe7
Alexander Thoren <thoren.alex@gmail.com>
2022-02-16 22:37:09 +0100

    :evergreen_tree: Updated DOM to fix background

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -11,38 +11,40 @@ export default function Settings() {
       </Head>
 
       <Header />
-      <section className="pt-20 w-full h-full text-white flex pr-[18px]">
-        <div className="w-max bg-black/50 p-2">
-          <ul className="p-2 flex flex-col items-center">
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => {
-              return i == 0 ? (
-                <li key={i} className="my-3 w-[72px] h-[72px] border-4 border-pink-500 rounded-full">
-                  <Image
-                    src="/images/icon.svg"
-                    alt="Dunhammer Logo"
-                    width={64}
-                    height={64}
-                  />
-                </li>
-              ) : (
-                <li key={i} className="my-3 w-16 h-16">
-                  <Image
-                    src="/images/icon.svg"
-                    alt="Dunhammer Logo"
-                    width={64}
-                    height={64}
-                  />
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-        <div className="m-8 rounded-2xl sticky w-full bg-black/50 flex justify-center items-center">
+      <main className="section-container">
+        <section className="pt-20 w-full h-full text-white flex pr-[18px]">
+          <div className="w-max bg-black/50 p-2">
+            <ul className="p-2 flex flex-col items-center">
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => {
+                return i == 0 ? (
+                  <li key={i} className="my-3 w-[72px] h-[72px] border-4 border-pink-500 rounded-full">
+                    <Image
+                      src="/images/icon.svg"
+                      alt="Dunhammer Logo"
+                      width={64}
+                      height={64}
+                    />
+                  </li>
+                ) : (
+                  <li key={i} className="my-3 w-16 h-16">
+                    <Image
+                      src="/images/icon.svg"
+                      alt="Dunhammer Logo"
+                      width={64}
+                      height={64}
+                    />
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <div className="m-8 rounded-2xl sticky w-full bg-black/50 flex justify-center items-center">
             <div className="m-2 bg-black">
               what
             </div>
-        </div>
-      </section>
+          </div>
+        </section>
+      </main>
       <Footer />
     </>
   )
