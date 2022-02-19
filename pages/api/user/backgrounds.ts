c86a0f177d38cda2ee1491ed5819f8b6face115b
Alexander Thoren <thoren.alex@gmail.com>
2022-02-19 17:47:31 +0100

    :alien: Isolated user backgrounds into separate API endpoint
    Minimizing the amount of data each endpoint returns

--- pages/api/user/backgrounds.ts
+++ pages/api/user/backgrounds.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
-import query from '../../lib/db'
-import { DBUser, ExtendedSession } from '../../lib/types'
+import query from '../../../lib/db'
+import type { ExtendedSession, DBUserBackground } from '../../../lib/types'
 
 export default async function handler(
     req: NextApiRequest,
@@ -21,11 +21,11 @@ export default async function handler(
 
     try {
         const result = ((await query({
-            query: "SELECT * FROM `users` WHERE `id` = ?",
+            query: "SELECT `bg`.`id`, `bg`.`name`, `bg`.`description`, `price`, `thumbnail`, `image`, `selected` FROM `users` as `usr` JOIN `user_background` as `usr_bg` ON `usr_bg`.`user_id` = `usr`.`id` JOIN `backgrounds` as `bg` ON `bg`.`id` = `usr_bg`.`background_id` WHERE `usr`.`id` = ?",
             values: [session.user.discordId]
-        })) as Array<DBUser>)
+        })) as Array<DBUserBackground>)
 
-        res.status(200).json({ data: result[0] })
+        res.status(200).json({ data: result })
     } catch (error) {
         res.status(500).json({ error })
     }
